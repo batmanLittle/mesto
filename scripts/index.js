9b458897bff3e94885bf82d9b7b95a83112f055d
Little Batman <fen10wa@yandex.ru>
2022-12-25 17:56:34 +0300

    Исправила вложенность по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ const profileTittle = document.querySelector(`.profile__title`);
 const profileSubtittle = document.querySelector(`.profile__subtitle`);
 
 const popupProfileButtonCard = document.querySelector(`.profile__button`);
-const popupCard = document.querySelector(`.popup-сard`);
+const popupCard = document.querySelector(`.popup_сard`);
 const buttonClosedCard = popupCard.querySelector(`.popup__close`);
 
 const popupImage = document.querySelector(`.popup_image`);
